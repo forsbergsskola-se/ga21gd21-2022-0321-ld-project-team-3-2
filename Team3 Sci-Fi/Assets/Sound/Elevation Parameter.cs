6ac254ba08a624069df7245a09f9fdc875485429
RobertPrinta <robert.larsson@student.forsbergsskola.se>
2022-04-06 16:42:05 +0200

    fmood

---
+++ Team3 Sci-Fi/Assets/Sound/Elevation Parameter.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// Behovet av att veta vart spelare & bilen är på Y axen
+// och att de värdet skalas och sen sickar de till fmood Parametern "Elevation Y axen"
+
+public class ElevationParameter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+     //Call Player/car Transfom Position Y   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Updater Fmod Parametern
+    }
+}
