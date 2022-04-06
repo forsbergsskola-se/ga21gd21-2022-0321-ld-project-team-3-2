c335ec63c9bc82d330ef5b1fe77743b28e953b87
Max Deurell Kent <max.deurell.kent@student.forsbergsskola.se>
2022-04-06 17:18:00 +0200

    Prefabs for damage zone and player health controller

--- Team3 Sci-Fi/Assets/Sound/Elevation Parameter.cs
+++ Team3 Sci-Fi/Assets/Sound/Elevation Parameter.cs
@@ -6,10 +6,24 @@ using UnityEngine;
 
 public class ElevationParameter : MonoBehaviour
 {
+    //kod exempel från max ;)
+    
+    //public GameObject player
+    //private Vector3 playerPosition
+    
+    //i start
+    
+    //playerPosition = player.position
+    
+    //sen kan man hämta y värdet med playerPosition.y
+    
+    
+    
     // Start is called before the first frame update
     void Start()
     {
      //Call Player/car Transfom Position Y   
+     
     }
 
     // Update is called once per frame
