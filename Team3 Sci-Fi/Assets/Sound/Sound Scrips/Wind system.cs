eb9d4a28ee6c7a41726c97053f5549a89b30f2bf
RobertPrinta <robert.larsson@student.forsbergsskola.se>
2022-04-07 13:55:17 +0200

    wind system and rpm

---
+++ Team3 Sci-Fi/Assets/Sound/Sound Scrips/Wind system.cs
@@ -0,0 +1,21 @@
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class NewBehaviourScript : MonoBehaviour
+//{
+    // Start is called before the first frame update
+    //void Start()
+    //{
+    // hur långt är man från stormen = Value
+    //}
+
+    // Update is called once per frame
+    //void Update()
+    // updatera Parametern 
+    //FMODUnity.RuntimeManager.StudioSystem.setParameterByName("WindIntesty", value);
+
+    //{
+        
+    //}
+//}
