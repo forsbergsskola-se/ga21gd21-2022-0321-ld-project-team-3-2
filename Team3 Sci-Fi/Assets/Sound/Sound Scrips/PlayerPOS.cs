b78e0c82d03a5932cd7d63548cc0acb8ecf1ad5f
Ludvigfrendin <ludvig.frendin@student.forsbergsskola.se>
2022-04-07 18:56:56 +0200

    försök på script

---
+++ Team3 Sci-Fi/Assets/Sound/Sound Scrips/PlayerPOS.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//public class PlayerPOS : MonoBehaviour
+//{
+    // Start is called before the first frame update
+ //   void Start() 
+  //  {
+        //Vector3 pos = transform.position;
+  //  }
+
+    // Update is called once per frame
+   // void Update()
+
+   // {
+        //Debug.Log(transform.position.y);
+    //}
+//}
