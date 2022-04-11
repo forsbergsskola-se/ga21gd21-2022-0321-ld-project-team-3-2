4532893b80abc49801b9afd6b2a7af530a3b2e76
Hugo Molander <hugo.molander@gmail.com>
2022-04-11 10:26:04 +0200

    vehicle

--- Team3 Sci-Fi/Assets/Designers/Hugo M/HMVehicleController.cs
+++ Team3 Sci-Fi/Assets/Designers/Hugo M/HMVehicleController.cs
@@ -18,11 +18,25 @@ public class HMVehicleController : MonoBehaviour
     [SerializeField] private WheelCollider backLeftCollider;
     [SerializeField] private WheelCollider backRightCollider;
     
+    // [SerializeField] private WheelCollider frontLeftCollider2;
+    // [SerializeField] private WheelCollider frontRightCollider2;
+    // [SerializeField] private WheelCollider backLeftCollider2;
+    // [SerializeField] private WheelCollider backRightCollider2;
+    // [SerializeField] private WheelCollider middleBackRightWheel2;
+    // [SerializeField] private WheelCollider middleBackLeftWheel2;
+    
     [SerializeField] private Transform frontLeftTransform;
     [SerializeField] private Transform frontRightTransform;
     [SerializeField] private Transform backLeftTransform;
     [SerializeField] private Transform backRightTransform;
-
+   
+    // [SerializeField] private Transform frontLeftTransform2;
+    // [SerializeField] private Transform frontRightTransform2;
+    // [SerializeField] private Transform backLeftTransform2;
+    // [SerializeField] private Transform backRightTransform2;
+    // [SerializeField] private Transform middleBackRightWheelTransform2;
+    // [SerializeField] private Transform middleBackLeftWheelTransform2;
+        
     [SerializeField] private float speed;
     [SerializeField] private float brakeForce;
     [SerializeField] private float maxSteeringAngle;
@@ -62,13 +76,20 @@ public class HMVehicleController : MonoBehaviour
         UpdateSingleWheel(frontRightCollider, frontRightTransform);
         UpdateSingleWheel(backRightCollider, backRightTransform);
         UpdateSingleWheel(backLeftCollider, backLeftTransform);
+      
+        // UpdateSingleWheel(frontLeftCollider2, frontLeftTransform2);
+        // UpdateSingleWheel(frontRightCollider2, frontRightTransform2);
+        // UpdateSingleWheel(backRightCollider2, backRightTransform2);
+        // UpdateSingleWheel(backLeftCollider2, backLeftTransform2);
+        // UpdateSingleWheel(middleBackRightWheel2,middleBackRightWheelTransform2);
+        // UpdateSingleWheel(middleBackLeftWheel2,middleBackLeftWheelTransform2);
     }
 
-    private void UpdateSingleWheel(WheelCollider wheelCollider, Transform wheelTransform)
+    private void UpdateSingleWheel(WheelCollider WheelCollider, Transform wheelTransform)
     {
         Vector3 position;
         Quaternion rotation;
-        wheelCollider.GetWorldPose(out position, out rotation);
+        WheelCollider.GetWorldPose(out position, out rotation);
         wheelTransform.rotation = rotation;
         wheelTransform.position = position;
     }
