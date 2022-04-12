217f4a286e12b8a427f88f6c04deba12d541d238
Max Deurell Kent <max.deurell.kent@student.forsbergsskola.se>
2022-04-12 15:05:26 +0200

    Updates to vehicle

--- Team3 Sci-Fi/Assets/Designers/Hugo M/HMVehicleController.cs
+++ Team3 Sci-Fi/Assets/Designers/Hugo M/HMVehicleController.cs
@@ -18,24 +18,24 @@ public class HMVehicleController : MonoBehaviour
     [SerializeField] private WheelCollider backLeftCollider;
     [SerializeField] private WheelCollider backRightCollider;
     
-    // [SerializeField] private WheelCollider frontLeftCollider2;
-    // [SerializeField] private WheelCollider frontRightCollider2;
-    // [SerializeField] private WheelCollider backLeftCollider2;
-    // [SerializeField] private WheelCollider backRightCollider2;
-    // [SerializeField] private WheelCollider middleBackRightWheel2;
-    // [SerializeField] private WheelCollider middleBackLeftWheel2;
+    [SerializeField] private WheelCollider frontLeftCollider2;
+    [SerializeField] private WheelCollider frontRightCollider2;
+    [SerializeField] private WheelCollider backLeftCollider2;
+    [SerializeField] private WheelCollider backRightCollider2;
+    [SerializeField] private WheelCollider middleBackRightWheel2;
+    [SerializeField] private WheelCollider middleBackLeftWheel2;
     
     [SerializeField] private Transform frontLeftTransform;
     [SerializeField] private Transform frontRightTransform;
     [SerializeField] private Transform backLeftTransform;
     [SerializeField] private Transform backRightTransform;
    
-    // [SerializeField] private Transform frontLeftTransform2;
-    // [SerializeField] private Transform frontRightTransform2;
-    // [SerializeField] private Transform backLeftTransform2;
-    // [SerializeField] private Transform backRightTransform2;
-    // [SerializeField] private Transform middleBackRightWheelTransform2;
-    // [SerializeField] private Transform middleBackLeftWheelTransform2;
+    [SerializeField] private Transform frontLeftTransform2;
+    [SerializeField] private Transform frontRightTransform2;
+    [SerializeField] private Transform backLeftTransform2;
+    [SerializeField] private Transform backRightTransform2;
+    [SerializeField] private Transform middleBackRightWheelTransform2;
+    [SerializeField] private Transform middleBackLeftWheelTransform2;
         
     [SerializeField] private float speed;
     [SerializeField] private float brakeForce;
@@ -77,12 +77,12 @@ public class HMVehicleController : MonoBehaviour
         UpdateSingleWheel(backRightCollider, backRightTransform);
         UpdateSingleWheel(backLeftCollider, backLeftTransform);
       
-        // UpdateSingleWheel(frontLeftCollider2, frontLeftTransform2);
-        // UpdateSingleWheel(frontRightCollider2, frontRightTransform2);
-        // UpdateSingleWheel(backRightCollider2, backRightTransform2);
-        // UpdateSingleWheel(backLeftCollider2, backLeftTransform2);
-        // UpdateSingleWheel(middleBackRightWheel2,middleBackRightWheelTransform2);
-        // UpdateSingleWheel(middleBackLeftWheel2,middleBackLeftWheelTransform2);
+        UpdateSingleWheel(frontLeftCollider2, frontLeftTransform2);
+        UpdateSingleWheel(frontRightCollider2, frontRightTransform2);
+        UpdateSingleWheel(backRightCollider2, backRightTransform2);
+        UpdateSingleWheel(backLeftCollider2, backLeftTransform2);
+        UpdateSingleWheel(middleBackRightWheel2,middleBackRightWheelTransform2);
+        UpdateSingleWheel(middleBackLeftWheel2,middleBackLeftWheelTransform2);
     }
 
     private void UpdateSingleWheel(WheelCollider WheelCollider, Transform wheelTransform)
