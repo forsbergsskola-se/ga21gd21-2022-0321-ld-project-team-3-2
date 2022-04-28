c125690136253c6391931d169bd29c71f8f1e155
jackahlberg <ahlberg.jack@gmail.com>
2022-04-28 14:00:41 +0200

    Adds toggle car lights on and off

--- Team3 Sci-Fi/Assets/Scripts/Player stuff/Flashlight.cs
+++ Team3 Sci-Fi/Assets/Scripts/Player stuff/Flashlight.cs
@@ -1,20 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Flashlight : MonoBehaviour
 {
     public Light flashlight;
+    public Light carLightUp;
+    public Light carLightDown;
+    public EnterOrExitVehicle InCar;
     
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.F) && flashlight.enabled == false)
+        if (Input.GetKeyDown(KeyCode.F) && flashlight.enabled == false && InCar.inCar == false)
         {
             flashlight.enabled = true;
         }
-        else if (Input.GetKeyDown(KeyCode.F) && flashlight.enabled)
+        else if (Input.GetKeyDown(KeyCode.F) && flashlight.enabled && InCar.inCar == false)
         {
             flashlight.enabled = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.F) && carLightUp.enabled == false && InCar.inCar)
+        {
+            carLightUp.enabled = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.F) && carLightUp.enabled && InCar.inCar)
+        {
+            carLightUp.enabled = false;
+        }
+        
+        if (Input.GetKeyDown(KeyCode.F) && carLightDown.enabled == false && InCar.inCar)
+        {
+            carLightDown.enabled = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.F) && carLightDown.enabled && InCar.inCar)
+        {
+            carLightDown.enabled = false;
         }
     }   
 }
