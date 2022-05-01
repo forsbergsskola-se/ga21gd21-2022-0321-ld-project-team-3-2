3eb27964c9cf765d351b0fadbe700f11eb9cd2ff
Joakimblom <joablo90@outlook.com>
2022-05-01 16:25:19 +0200

    Added something to the car, YOU MUST PLAY TO SEE <3

---
+++ Team3 Sci-Fi/Assets/Designers/LevelDesign-Jocke/LD-Scripts/particleWheel.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+
+public class particleWheel : MonoBehaviour
+{
+    
+    [SerializeField] private GameObject pBack;
+    [SerializeField] private ParticleSystem dustParticles;
+    [SerializeField] private float speedThreshold;
+   
+    private Rigidbody pbackmyrb;
+   
+    private bool particlesPlaying;
+    private bool goingForward;
+    private bool goingBackward;
+    private bool belowThreshold;
+    private Vector3 velocity;
+    private void Start()
+    {
+        pbackmyrb = pBack.GetComponent<Rigidbody>();
+    }
+    private void Update()
+    {
+        //We check if we are spawning particles or not
+        //Checking speed value of rigid body
+        velocity = pbackmyrb.velocity;
+      
+        //checking speed value is greater than threshold
+        goingForward = !particlesPlaying && (velocity.sqrMagnitude > speedThreshold);
+      
+        //checking speed value is lesser than threshold
+        goingBackward = !particlesPlaying && (velocity.sqrMagnitude < -speedThreshold);
+      
+        //checking speed value is within positive & negative threshold 
+        belowThreshold = particlesPlaying &&
+                         (velocity.sqrMagnitude < speedThreshold && velocity.sqrMagnitude > -speedThreshold);
+      
+        if (goingForward || goingBackward)
+        {
+            particlesPlaying = true;
+            SpawnParticles();
+        }
+        else if(belowThreshold) 
+        {
+            particlesPlaying = false;
+            Debug.Log("stopping particles");
+            dustParticles.Stop();
+        }
+    }
+    private void SpawnParticles()
+    {
+        Debug.Log("Starting particles");
+        dustParticles.Play();
+    }
+}
