5d263953762a6cc5464a42f19b2e4972896dd342
jackahlberg <ahlberg.jack@gmail.com>
2022-05-05 11:01:18 +0200

    Creates baseline of script and makes it toggle on input.

--- Team3 Sci-Fi/Assets/Designers/Jack/QuestWaypoint.cs
+++ Team3 Sci-Fi/Assets/Designers/Jack/QuestWaypoint.cs
@@ -1,18 +1,33 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class QuestWaypoint : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Image questMarker;
+    public Transform target;
+
+    private void Start()
     {
-        
+        questMarker.enabled = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        questMarker.transform.position = Camera.main.WorldToScreenPoint(target.position);
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            StartCoroutine(ToggleQuestMarker());
+        }
+    }
+
+    IEnumerator ToggleQuestMarker()
+    {
+        questMarker.enabled = true;
+        yield return new WaitForSeconds(4);
+        questMarker.enabled = false;
     }
 }
