d417e38c65b1475e600edd0846610bc79c6537ef
jackahlberg <ahlberg.jack@gmail.com>
2022-05-05 11:21:21 +0200

    Finishes quest marker script and adds to player prefab

--- Team3 Sci-Fi/Assets/Designers/Jack/QuestWaypoint.cs
+++ Team3 Sci-Fi/Assets/Designers/Jack/QuestWaypoint.cs
@@ -16,7 +16,30 @@ public class QuestWaypoint : MonoBehaviour
 
     void Update()
     {
-        questMarker.transform.position = Camera.main.WorldToScreenPoint(target.position);
+        float minX = questMarker.GetPixelAdjustedRect().width / 2;
+        float maxX = Screen.width - minX;
+        
+        float minY = questMarker.GetPixelAdjustedRect().height / 2;
+        float maxY = Screen.width - minY;
+        
+        Vector2 pos = Camera.main.WorldToScreenPoint(target.position);
+
+        if(Vector3.Dot((target.position - transform.position), transform.forward) < 0)
+        {
+            if(pos.x < Screen.width / 2)
+            {
+                pos.x = maxX;
+            }
+            else
+            {
+                pos.x = minX;
+            }
+        }
+        
+        pos.x = Mathf.Clamp(pos.x, minX, maxX);
+        pos.y = Mathf.Clamp(pos.y, minY, maxY);
+        
+        questMarker.transform.position = pos;
 
         if (Input.GetKeyDown(KeyCode.Q))
         {
