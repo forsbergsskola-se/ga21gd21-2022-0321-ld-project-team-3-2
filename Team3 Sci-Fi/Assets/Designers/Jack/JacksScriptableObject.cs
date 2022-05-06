7ca09b67353e6b8f461505104929ad054a7a63a1
MaxDKCrab <max.deurell.kent@student.forsbergsskola.se>
2022-05-06 14:28:53 +0200

    Added jacks roliga kub and smoke progress

---
+++ Team3 Sci-Fi/Assets/Designers/Jack/JacksScriptableObject.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "New rolig kub", menuName = "Jacks roliga kub")]
+public class JacksScriptableObject : ScriptableObject
+{
+    // lägg aray här
+    //och här ifall du ska ha fler
+    
+    
+}
