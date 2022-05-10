9756ef4e3d8dc369306eb62bdc48dabef622619c
jackahlberg <ahlberg.jack@gmail.com>
2022-05-10 12:22:10 +0200

    Finished Puzzle 2 timer

--- Team3 Sci-Fi/Assets/Designers/Jack/Scripts/Puzzle2Timer.cs
+++ Team3 Sci-Fi/Assets/Designers/Jack/Scripts/Puzzle2Timer.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Puzzle2Trigger : MonoBehaviour
+public class Puzzle2Timer : MonoBehaviour
 {
 
     [SerializeField] private QuestPuzzle2 waypoints;
