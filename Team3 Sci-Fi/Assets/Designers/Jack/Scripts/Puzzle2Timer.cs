be83b995f00b062e8b66dc993622bed03fb88361
jackahlberg <ahlberg.jack@gmail.com>
2022-05-10 12:19:52 +0200

    Adds timer to puzzle 2 (WIP)

---
+++ Team3 Sci-Fi/Assets/Designers/Jack/Scripts/Puzzle2Timer.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puzzle2Trigger : MonoBehaviour
+{
+
+    [SerializeField] private QuestPuzzle2 waypoints;
+    private Collider thisColl;
+    private int waitForSeconds = 5;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        StartCoroutine(StartTimer());
+        Debug.Log("Timer Started");
+    }
+
+    IEnumerator StartTimer()
+    {
+        yield return new WaitForSeconds(waitForSeconds);
+        waypoints.enabled = true;
+    }
+}
