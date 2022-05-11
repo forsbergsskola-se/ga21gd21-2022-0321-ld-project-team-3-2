4c5e0f9f24019077ea4db0b3cd81a0bf852e3eaa
Hugo Molander <hugo.molander@gmail.com>
2022-05-11 14:23:26 +0200

    Added "AirstrafeMovement"
    script that increases speed when player is in the air and reverts to previous speed when grounded. Creates the sensation of air strafing and allows more control over the character while in the air.

---
+++ Team3 Sci-Fi/Assets/Designers/Hugo M/AirstrafeMovementHM.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+
+public class AirstrafeMovementHM : MonoBehaviour
+{
+    [SerializeField] private CharacterController controller;
+    public float speed;
+    public float sprintSpeed;
+    private Vector3 velocity;
+    [SerializeField] private float gravity = -9.81f;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float groundDistance = 0.4f;
+    [SerializeField] private LayerMask groundMask;
+    public bool isGrounded;
+    public bool jumpOn;
+    [SerializeField] private float jumpForce;
+
+    
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        if (isGrounded && Input.GetButtonDown("Jump") && jumpOn)
+        {
+            velocity.y += jumpForce;
+        }
+        
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            controller.Move(move * sprintSpeed * Time.deltaTime);
+        }
+        else
+        {
+            controller.Move(move * speed * Time.deltaTime);
+        }
+        
+       
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+
+        if (isGrounded == false)
+        {
+            speed = 20;
+        }
+        else
+        {
+            speed = 10;
+        }
+    }
+}
