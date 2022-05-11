b99b1628777c3b787db775c85a61498e976de070
jackahlberg <ahlberg.jack@gmail.com>
2022-05-11 17:00:13 +0200

    Tries fixing vehicle waypoint bug (not working)

--- Team3 Sci-Fi/Assets/Designers/Jack/Scripts/QuestWaypoint.cs
+++ Team3 Sci-Fi/Assets/Designers/Jack/Scripts/QuestWaypoint.cs
@@ -26,44 +26,42 @@ public class QuestWaypoint : MonoBehaviour
 
     void Update()
     {
-        if (isInCar.inCar)
+        while (isInCar.inCar)
         {
-
+            questMarker.enabled = false;
+            meter.enabled = false;
+            activeQuest.enabled = false;
         }
-        else
-        {
-            float minX = questMarker.GetPixelAdjustedRect().width / 2;
-            float maxX = Screen.width - minX;
-            float minY = questMarker.GetPixelAdjustedRect().height / 2;
-            float maxY = Screen.width - minY;
+        float minX = questMarker.GetPixelAdjustedRect().width / 2;
+        float maxX = Screen.width - minX;
+        float minY = questMarker.GetPixelAdjustedRect().height / 2;
+        float maxY = Screen.width - minY;
         
-            Vector2 pos = Camera.main.WorldToScreenPoint(target[targetArrayValue].position);
+        Vector2 pos = Camera.main.WorldToScreenPoint(target[targetArrayValue].position);
 
-            if(Vector3.Dot((target[targetArrayValue].position - transform.position), transform.forward) < 0)
-            {
-                if(pos.x < Screen.width / 2)
-                {
-                    pos.x = maxX;
-                }
-                else
-                {
-                    pos.x = minX;
-                }
+        if(Vector3.Dot((target[targetArrayValue].position - transform.position), transform.forward) < 0)
+        { if(pos.x < Screen.width / 2)
+            { 
+                pos.x = maxX;
             }
-        
-            pos.x = Mathf.Clamp(pos.x, minX, maxX);
-            pos.y = Mathf.Clamp(pos.y, minY, maxY);
-        
-            questMarker.transform.position = pos;
-            meter.text = (int)Vector3.Distance(target[targetArrayValue].position, transform.position) + " m";
-            activeQuest.text = activeQuestString[activeQuestArrayValue];
-        
-            if (Input.GetKeyDown(KeyCode.Q))
+            else
             {
-                StartCoroutine(ToggleQuestMarker());
+                pos.x = minX;
             }
         }
-
+        
+        pos.x = Mathf.Clamp(pos.x, minX, maxX);
+        pos.y = Mathf.Clamp(pos.y, minY, maxY);
+        
+        questMarker.transform.position = pos;
+        meter.text = (int)Vector3.Distance(target[targetArrayValue].position, transform.position) + " m";
+        activeQuest.text = activeQuestString[activeQuestArrayValue];
+        
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            StartCoroutine(ToggleQuestMarker());
+        }
+            
     }
 
     IEnumerator ToggleQuestMarker()
