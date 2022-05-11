24568ffe8984c7f9dd42b6b6857e5040defb6e1d
Max Deurell Kent <max.deurell.kent@student.forsbergsskola.se>
2022-05-11 17:30:16 +0200

    Fix start of game bug, fixed harons collider

---
+++ Team3 Sci-Fi/Assets/Scripts/Dialogue/DialogueTriggerStartOfGame.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTriggerStartOfGame : MonoBehaviour
+{
+    public float dialogueRange;
+    private BoxCollider thisColl;
+    private DialogueManager dialogueManager;
+    public ChoiceDialogue choiceDialogue;
+    private float distance;
+    private GameObject player;
+    private InteractionManager interact;
+    public string interactMessage;
+    
+    void Start()
+    {
+        interact = FindObjectOfType<InteractionManager>();
+        player = FindObjectOfType<Movement>().gameObject;
+        choiceDialogue.isDialogueFinished = false;
+        dialogueManager = FindObjectOfType<DialogueManager>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        dialogueManager.StartDialogue(choiceDialogue);
+        choiceDialogue.talking = true;
+        thisColl.enabled = false;
+    }
+
+    private void Update()
+    {
+        distance = Vector3.Distance(player.transform.position, transform.position);
+        
+        if (distance <= dialogueRange && Input.GetKeyDown(KeyCode.E))
+        {
+            if (!dialogueManager.isTalking && !choiceDialogue.isDialogueFinished)
+            {
+                dialogueManager.StartDialogue(choiceDialogue);
+                choiceDialogue.talking = true;
+            }
+            else if (!dialogueManager.isTalking && choiceDialogue.isDialogueFinished)
+            {
+                dialogueManager.StartReturnMessageDialogue(choiceDialogue);
+                choiceDialogue.talking = true;
+            }
+            else if (dialogueManager.isTalking)
+            {
+                if (!dialogueManager.inChoice && choiceDialogue.isDialogueFinished)
+                {
+                    dialogueManager.DisplayReturnMessage();
+                }
+                else if (!dialogueManager.inChoice && !choiceDialogue.isDialogueFinished)
+                {
+                    dialogueManager.DisplayNextSentenceChoice();
+                }
+            }
+        }
+
+        if (distance > dialogueRange && dialogueManager.isTalking && choiceDialogue.talking)
+        {
+            dialogueManager.CancelDialogue();
+            choiceDialogue.talking = false;
+        }
+    }
+
+    private void OnMouseOver()
+    {
+        if (distance <= dialogueRange && !dialogueManager.isTalking && enabled)
+        {
+            interact.ShowInteractMessage(interactMessage);
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        interact.HideInteractMessage();
+    }
+}
