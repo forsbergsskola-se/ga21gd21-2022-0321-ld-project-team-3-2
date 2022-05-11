2ac7e98c93b12c5190ecf56930584c2707567e19
jackahlberg <ahlberg.jack@gmail.com>
2022-05-11 16:13:33 +0200

    Mostly finished for playable

---
+++ Team3 Sci-Fi/Assets/Designers/Jack/Scripts/ChangeActiveQuestTrigger.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeActiveQuestTrigger : MonoBehaviour
+{
+
+    [SerializeField] private Collider thisColl;
+    [SerializeField] public ChangeActiveQuest ca;
+    private void OnTriggerEnter(Collider other)
+    {
+        ca.IncramentActiveQuestExternal();
+        thisColl.enabled = false;
+    }
+}
