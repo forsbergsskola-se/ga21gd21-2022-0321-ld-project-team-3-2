eb9d9fd2c5202dcada705aaef06e8a2944e50302
Max Deurell Kent <max.deurell.kent@student.forsbergsskola.se>
2022-05-11 12:14:34 +0200

    New dmg indication and escape menu in main scene

--- Team3 Sci-Fi/Assets/Scripts/Player stuff/PlayerHealthManager.cs
+++ Team3 Sci-Fi/Assets/Scripts/Player stuff/PlayerHealthManager.cs
@@ -16,7 +16,6 @@ public class PlayerHealthManager : MonoBehaviour
     public float maxHealth = 100f;
     private bool isTakingDamage;
     public float healthRegenSpeed;
-    [SerializeField] private Image healthIndicator;
     private GameProgressionManager gameProgress;
     [SerializeField] private Transform player;
     [SerializeField] private Transform vehicleFront;
@@ -68,18 +67,7 @@ public class PlayerHealthManager : MonoBehaviour
             Death();
         }
         
-        if (currentHealth > maxHealth * 0.67f)
-        {
-            healthIndicator.color = Color.cyan;
-        }
-        else if (currentHealth > maxHealth * 0.34f && currentHealth < maxHealth * 0.67f)
-        {
-            healthIndicator.color = Color.yellow;
-        }
-        else
-        {
-            healthIndicator.color = Color.red;
-        }
+     
         RegenerateHealth();
         isTakingDamage = false;
     }
