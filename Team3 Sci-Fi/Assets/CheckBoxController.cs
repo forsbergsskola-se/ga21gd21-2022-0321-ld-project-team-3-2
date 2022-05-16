ac3ec7f152c319ca7b6fda91f88285a7062c5602
MaxDKCrab <max.deurell.kent@student.forsbergsskola.se>
2022-05-16 19:18:54 +0200

    Settings progress (animation and volume sliders left)

---
+++ Team3 Sci-Fi/Assets/CheckBoxController.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CheckBoxController : MonoBehaviour
+{
+    // [SerializeField] private Sprite checkedBox;
+    // [SerializeField] private Sprite uncheckedBox;
+    // private Image thisImage;
+    // public CheckBoxController[] otherChecksInSetting;
+    // public bool isChecked;
+    //
+    //
+    // private void Start()
+    // {
+    //     thisImage = GetComponent<Image>();
+    // }
+    //
+    // private void Update()
+    // {
+    //     if (Input.GetKeyDown(KeyCode.Mouse0))
+    //     {
+    //         for (int i = 0; i < otherChecksInSetting.Length; i++)
+    //         {
+    //             if (otherChecksInSetting[i].isChecked)
+    //             {
+    //                 isChecked = false;
+    //                 thisImage.sprite = uncheckedBox;
+    //             }
+    //         }
+    //     }
+    // }
+    //
+    //
+    // public void CheckBox()
+    // {
+    //     thisImage.sprite = checkedBox;
+    // }
+}
--- Team3 Sci-Fi/Assets/Scripts/General stuff/SettingsController.cs
+++ Team3 Sci-Fi/Assets/Scripts/General stuff/SettingsController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SettingsController : MonoBehaviour
 {
@@ -15,16 +16,59 @@ public class SettingsController : MonoBehaviour
     private MouseLook fpsView;
     private Movement playerMove;
     private bool inSettings;
+    public Image checkBox1920x1080;
+    public Image checkBox1600x900;
+    public Image checkBox2560x1440;
+    public Image checkBoxFullscreen;
+    public Image checkBoxWindowed;
+    public Sprite checkedBox;
+    public Sprite uncheckedBox;
 
     private void Start()
     {
         playerMove = FindObjectOfType<Movement>();
         healthManager = FindObjectOfType<PlayerHealthManager>();
         fpsView = FindObjectOfType<MouseLook>();
+        Screen.SetResolution(1920,1080,Screen.fullScreen);
+        
+        Screen.fullScreen = true;
+        
     }
 
     private void Update()
     {
+        if (Screen.fullScreen)
+        {
+            checkBoxWindowed.sprite = uncheckedBox;
+            checkBoxFullscreen.sprite = checkedBox;
+        }
+        else
+        {
+            checkBoxWindowed.sprite = checkedBox;
+            checkBoxFullscreen.sprite = uncheckedBox;
+        }
+
+        if (Screen.currentResolution.width == 1920)
+        {
+            checkBox1920x1080.sprite = checkedBox;
+            checkBox1600x900.sprite = uncheckedBox;
+            checkBox2560x1440.sprite = uncheckedBox;
+        }
+        else if (Screen.currentResolution.width == 1600)
+        {
+            checkBox1920x1080.sprite = uncheckedBox;
+            checkBox1600x900.sprite = checkedBox;
+            checkBox2560x1440.sprite = uncheckedBox;
+        }
+        else
+        {
+            checkBox1920x1080.sprite = uncheckedBox;
+            checkBox1600x900.sprite = uncheckedBox;
+            checkBox2560x1440.sprite = checkedBox;
+        }
+        
+        
+        
         if (Input.GetKeyDown(KeyCode.Escape) && !inMenu)
         {
             OpenMenu();
@@ -95,22 +139,27 @@ public class SettingsController : MonoBehaviour
     public void Resolution1920x1080()
     {
         FMODUnity.RuntimeManager.PlayOneShot("event:/Ui/Clicks");
+        Screen.SetResolution(1920,1080, Screen.fullScreen);
     }
     public void Resolution1600x900()
     {
         FMODUnity.RuntimeManager.PlayOneShot("event:/Ui/Clicks");
+        Screen.SetResolution(1600,900, Screen.fullScreen);
     }
     public void Resolution2560x1440()
     {
         FMODUnity.RuntimeManager.PlayOneShot("event:/Ui/Clicks");
+        Screen.SetResolution(2560,1440, Screen.fullScreen);
     }
     public void Fullscreen()
     {
         FMODUnity.RuntimeManager.PlayOneShot("event:/Ui/Clicks");
+        Screen.fullScreen = true;
     }
     public void Windowed()
     {
         FMODUnity.RuntimeManager.PlayOneShot("event:/Ui/Clicks");
+        Screen.fullScreen = false;
     }
     
     
