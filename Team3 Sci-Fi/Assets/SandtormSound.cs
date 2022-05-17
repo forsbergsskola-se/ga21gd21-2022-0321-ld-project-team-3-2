298713879976ce14ed245fcfc5170ecd28977d05
MaxDKCrab <max.deurell.kent@student.forsbergsskola.se>
2022-05-17 11:57:41 +0200

    Sandstorm sound

---
+++ Team3 Sci-Fi/Assets/SandtormSound.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SandtormSound : MonoBehaviour
+{
+
+    private Transform playerTrans;
+    private float distanceToStorm;
+    
+    
+    void Start()
+    {
+        playerTrans = FindObjectOfType<Movement>().gameObject.transform;
+    }
+
+    
+    void Update()
+    {
+
+        distanceToStorm = Vector3.Distance(playerTrans.position, transform.position);
+        distanceToStorm = Math.Clamp(distanceToStorm, 0, 100);
+        FMODUnity.RuntimeManager.StudioSystem.setParameterByName("DistensWind", distanceToStorm);
+
+
+    }
+}
