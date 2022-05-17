fcc68f05f2c2e71b4eca0463cb4476a9a92e2ec5
jackahlberg <ahlberg.jack@gmail.com>
2022-05-17 11:00:43 +0200

    ADds falling bridge animation script

---
+++ Team3 Sci-Fi/Assets/Designers/Jack/Scripts/StartBridgeFall.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Unity.VisualScripting;
+using UnityEngine;
+
+    public class StartBridgeFall : MonoBehaviour
+    {
+        [SerializeField] private Animator anim;
+        [SerializeField] private InteractionManager interact;
+
+        public bool withinRange;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            withinRange = true;
+            interact.ShowInteractMessage("Press E to interact");
+        }
+        
+        private void OnTriggerExit(Collider other)
+        {
+            interact.HideInteractMessage();
+            withinRange = false;
+        }
+        
+        private void Update()
+        {
+            if (!withinRange)
+            {
+                return;
+            }
+
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                anim.enabled = true;
+            }
+        }
+    }
