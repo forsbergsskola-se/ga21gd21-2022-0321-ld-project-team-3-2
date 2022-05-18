9bcd69602a2ce613d1fe16ba67f29a9cb7e95812
jackahlberg <ahlberg.jack@gmail.com>
2022-05-18 15:38:27 +0200

    Adds script for ending 4 and fills all scenes

--- Team3 Sci-Fi/Assets/Scripts/Story specifick mechanics/Ending4Text.cs
+++ Team3 Sci-Fi/Assets/Scripts/Story specifick mechanics/Ending4Text.cs
@@ -5,13 +5,14 @@ using TMPro;
 using UnityEngine;
 using FMOD.Studio;
 
-public class EndingText : MonoBehaviour
+public class Ending4Text : MonoBehaviour
 {
 
     [SerializeField] private TMP_Text[] text;
     [SerializeField] private Animator anim;
     private EventInstance typingSounds; 
     [TextArea(3, 10)] public string[] storyText = new string[10];
+    [TextArea(3, 10)] public string[] storyTextTwo = new string[10];
     private int index;
     public float waitForSeconds = 0.03f;
     void Start()
@@ -24,6 +25,36 @@ public class EndingText : MonoBehaviour
     IEnumerator TypeSentence (string[] sentence)
     {
         yield return new WaitForSeconds(5);
+        for (int i = 0; i < text.Length; i++)
+        {
+            text[i].text = "";
+            typingSounds.start();
+            foreach (char letter in sentence[index].ToCharArray())
+            {
+                text[i].text += letter;
+                yield return new WaitForSeconds(waitForSeconds);
+            }
+            index++;
+            typingSounds.stop(STOP_MODE.IMMEDIATE);
+            yield return new WaitForSeconds(1);
+        }
+
+        yield return new WaitForSeconds(2.5f);
+        anim.SetTrigger("Fade Out");
+        yield return new WaitForSeconds(1);
+        for (int i = 0; i < text.Length; i++)
+        {
+            text[i].text = "";
+        }
+
+        index = 0;
+        anim.SetTrigger("Fade In");
+        StartCoroutine(TypeSentenceTwo(storyTextTwo));
+    }
+    
+    IEnumerator TypeSentenceTwo (string[] sentence)
+    {
+        yield return new WaitForSeconds(2);
         for (int i = 0; i < text.Length; i++)
         {
             text[i].text = "";
